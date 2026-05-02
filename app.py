4ddf828a8fac76da5be19d8a1d9e0a15f8737d74
ritikabadhan26 <ritikabadhan26@gmail.com>
2026-05-02 23:57:06 +0530

    Add placeholder response to login route
    Added a placeholder response for the login route.

--- app.py
+++ app.py
@@ -75,6 +75,7 @@ def login_required(f):
 # AUTH
 @app.route('/login', methods=['GET','POST'])
 def login():
+    return"Logic page coming soon"
     if 'user_id' in session:
         return redirect(url_for('dashboard'))
     if request.method == 'POST':
